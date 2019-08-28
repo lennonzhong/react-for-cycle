037529a1cc5894bc4a006c737523e6ce019756ff
lennonzhong <804537383@qq.com>
2019-08-28 10:31:02 +0800

    项目配置

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { Button } from "antd";
+import './style/app.less';
 class App extends Component {
     constructor(props) {
         super(props);
@@ -7,7 +8,9 @@ class App extends Component {
     }
     render() { 
         return ( 
-            <div className="test"></div>
+            <div className="test">
+                <Button type="primary">按钮1</Button>  
+            </div>
          );
     }
 }
