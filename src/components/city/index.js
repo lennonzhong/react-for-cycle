d8b73fb5a4dd98e964e38a814381cee0fe193a75
lennonzhong <804537383@qq.com>
2019-11-16 16:43:47 +0800

    tijiao

--- src/components/city/index.js
+++ src/components/city/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {Form, Button,Select, Input, Card,Table } from 'antd';
+import {Form, Button,Select, Input, Card,Table , Modal} from 'antd';
+import axios from '../../utils/axios'
 const FormItem = Form.Item;
 export default class City extends Component{
     constructor(props) {
@@ -60,12 +61,37 @@ export default class City extends Component{
                 onChange: this.pageChange.bind(this),
                 onShowSizeChange: this.onShowSizeChange
             }
-        }
+        };
+        this.modalVisible = false;
     }
     componentDidMount() {
-        
+        this.request();
     }
-
+    request() {
+        let {page, pageSize} = this.state.pagination;
+        axios.$ajax({
+            url: '/open_city',
+            data:{
+                params:{
+                    'page': page,
+                    'size': pageSize
+                }
+            }
+        }).then((res)=>{
+            let list = res.result.item_list.map((item, index) => {
+                item.key = index;
+                return item;
+            });
+            let {page, pageSize} = res.result;
+            this.setState({
+                tableData:list,
+                pagination: {
+                    ...this.state.pagination,
+                    page, pageSize
+                }
+            })
+        })
+    }
     onShowSizeChange=(current, size)=>{
         let temp = this.state.pagination;
         var obj = Object.assign({}, temp, {
@@ -95,18 +121,105 @@ export default class City extends Component{
                     <FilterForm></FilterForm>
                 </Card>
 
-                <Card>
-                    <Button>开通城市</Button>
-                </Card>
-
-                <Card title="城市管理" style={{marginTop: 20}}>
+                <Card style={{marginTop: 20}}>
+                    <Button onClick={this.openDialog}>开通城市</Button>
                     <Table columns={this.state.columns} dataSource={this.state.tableData} pagination={this.state.pagination}></Table>
                 </Card>
+
+                <Modal title="开通城市" visible={this.state.modalVisible} onOk={this.handleOk} onCancel={this.handleCancel} >
+                    <ModelsForm wrappedComponentRef={(ref)=> {this.cityForm = ref}}></ModelsForm>
+                </Modal>
             </div>
         )
     }
+
+    handleOk=()=> {
+        // todo validate and submit
+        // let cityInfo = this.cityForm.props.form.getFieldsValue();
+        // 
+        this.cityForm.props.form.validateFields((err, values) => {
+              if (!err) {
+                console.log('Received values of form: ', values);
+                // ajax
+              }
+            })
+
+        // this.props.form.validateFields((err, values) => {
+        //   if (!err) {
+        //     console.log('Received values of form: ', values);
+        //   }
+        // });
+    }
+    handleCancel=()=>  {
+        this.setState({
+            modalVisible: false
+        })
+    }
+    openDialog=()=>  {
+        this.setState({
+            modalVisible: true
+        })
+    }
 }
 
+class ModelForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() {
+        let formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 8 },
+              },
+              wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 10 },
+              }
+        }
+
+        let {getFieldDecorator} = this.props.form;
+        return ( 
+            <div>
+                  <Form {...formItemLayout} className="login-form">
+                        <Form.Item label="选择城市">
+                            {
+                                getFieldDecorator('city',{
+                                    initialValue: ''
+                                })(
+                                    <Select
+                                    placeholder="全部"
+                                    >
+                                        <Select.Option value="">全部</Select.Option>
+                                        <Select.Option value="1">北京市</Select.Option>
+                                        <Select.Option value="2">天津市</Select.Option>
+                                        <Select.Option value="3">深圳市</Select.Option>
+                                    </Select>
+                                )
+                            }
+                        </Form.Item>
+                        <Form.Item label="运营模式">
+                            {
+                                getFieldDecorator('mode',{
+                                    initialValue: '1'
+                                })(
+                                    <Select
+                                    placeholder="模式"
+                                    >
+                                        <Select.Option value="1">自营</Select.Option>
+                                        <Select.Option value="2">合作</Select.Option>
+                                        <Select.Option value="3">禁停</Select.Option>
+                                    </Select>
+                                )
+                            }
+                        </Form.Item>
+                  </Form>
+            </div>
+        );
+    }
+}
+const ModelsForm = Form.create({})(ModelForm);
 
 class FilterFormClass extends Component{
     constructor(props){
